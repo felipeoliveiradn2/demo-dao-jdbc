ad4303ad893e492618ecf072dff7b41881298679
unknown <felipeoliveiradn2@gmail.com>
2025-08-25 20:45:19 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -24,13 +24,68 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-	}
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+					
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+					
+			int rowsAffected = st.executeUpdate();
+			
+				if(rowsAffected > 0) {
+					ResultSet rs = st.getGeneratedKeys();
+					if (rs.next()) {
+						int id = rs.getInt(1);
+						obj.setId(id);
+					}
+					DB.closeResultSet(rs);
+				}
+				else {
+					throw new DbException("Unexpected error! No rows Affected!"); 
+				}
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			finally {
+				DB.closeStatement(st);
+			}
+		}
+	
+	
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-	}
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement ()
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?"}
+					+ "WHERE Id = ?"); 
+					
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+							
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}	
 
 	@Override
 	public void deleteById(Integer id) {
@@ -87,6 +142,9 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public List<Seller> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
 		try {
 			st = conn.prepareStatement(
 					"SELECT seller.*,department.Name as DepName "
